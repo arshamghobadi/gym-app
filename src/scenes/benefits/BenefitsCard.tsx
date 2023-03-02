9efa5ef88b7da8b7554900c3c115faaa1aa1b559
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-02 17:15:40 +0000

    benefits page compelete

--- src/scenes/benefits/BenefitsCard.tsx
+++ src/scenes/benefits/BenefitsCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 type Props = {
   data: {
     icon: JSX.Element;
@@ -9,5 +7,13 @@ type Props = {
 };
 
 export default function BenefitsCard({ data }: Props) {
-  return <div>BenefitsCard</div>;
+  return (
+    <div className="flex flex-col p-5 items-center space-y-5 mt-5 border-2 border-gray-300 rounded-lg font-semibold">
+      <div className="flex border-2 border-stone-400 bg-red-300 p-4 rounded-full ">
+        {data.icon}
+      </div>
+      <div>{data.title}</div>
+      <div>{data.description}</div>
+    </div>
+  );
 }
