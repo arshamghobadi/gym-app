47561d5b6121794c5092e8e4ee0b40fb1a026786
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-02 17:15:46 +0000

    a

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -2,7 +2,7 @@ import { SelectedPage } from '../../shared/types';
 import { motion } from 'framer-motion';
 import HText from '../../shared/HText';
 import BenefitsCard from './BenefitsCard';
-
+import BenefitsPageGraphic from '../../assets/BenefitsPageGraphic.png';
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
   benefitsdata: Array<{
@@ -24,12 +24,47 @@ function Benefits({ setSelectedPage, benefitsdata }: Props) {
             into each and every member.
           </p>
         </div>
-        <div>
+        <div className="flex flex-col p-2 space-y-4 sm:max-w-xl sm:mx-auto md:flex md:flex-row md:items-center md:space-x-10 md:max-w-6xl">
           {benefitsdata.map((item) => (
             <BenefitsCard key={item.title} data={item} />
           ))}
         </div>
       </motion.div>
+      <div>
+        <img
+          className="flex mx-auto"
+          src={BenefitsPageGraphic}
+          alt="benefitsPicture"
+        />
+      </div>
+      <div className="p-5 max-w-6xl">
+        <HText>
+          MILLIONS OF HAPPY MEMBERS GETTING{' '}
+          <span className=" text-red-400">FIT.</span>
+        </HText>
+      </div>
+      <div className=" font-semibold space-y-3 p-5">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil totam
+          deleniti ad dolore iure maiores corporis reiciendis? Voluptatum
+          voluptate cumque rerum veritatis. Pariatur sit, odit cupiditate ea
+          iusto hic perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Illum molestiae minima veritatis atque pariatur ex
+          deserunt, enim laudantium libero! Harum aliquid voluptatum
+          reprehenderit numquam quam esse cumque beatae eate{' '}
+        </p>
+        <p>
+          magnam, nostrum voluptatibus corrupti fugit vero assumenda commodi
+          velit. Architecto incidunt a libero repellendus nam praesentium itaque
+          excepturi sapiente consequatur dolore dolorem corporis quo, sunt
+          facilis. Obcaecati, tempora. Distinctio illum dicta aliquid, dolorem
+          cupiditate doloribus vitae sunt aliquam a, aperiam fugit maiores, et
+          quaerat corrupti. Eaque delectus libero animi, id ipsam explicabo
+          dicta. Enim, blanditiis. Voluptatibus sunt amet eos esse sed?
+          Similique, voluptas cumque voluptates laborum dolorem soluta quisquam
+          dolor reprehenderit.
+        </p>
+      </div>
     </section>
   );
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -21,109 +21,105 @@ export default function Navbar({
   const navbarBackGruond = isTopOfPage ? '' : 'bg-pink-300';
   const flexBetween = 'flex items-center justify-between';
   return (
-    <nav className={`${navbarBackGruond} `}>
-      <div>
+    <nav className={`${navbarBackGruond} w-full mx-auto items-center`}>
+      <div className="flex items-center justify-between p-5 max-w-6xl mx-auto">
+        <img alt="logo" src={Logo} />
         <div>
-          <div className="flex items-center justify-between p-5">
-            <img alt="logo" src={Logo} />
-            <div>
-              <div className="hidden md:inline-flex gap-5 mr-5">
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+          <div className="hidden md:inline-flex gap-5 mr-5">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
 
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
 
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
 
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </div>
-              <div className={`${flexBetween} gap-8  hidden md:inline-flex `}>
-                <p>sign in</p>
-                <ActionButton setSelectedPage={setSelectedPage}>
-                  Become a member
-                </ActionButton>
-              </div>
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                className=" rounded-full bg-yellow-500 p-2 md:hidden"
-              >
-                <HiBars3 className="h-6 w-6 text-white" />
-              </button>
-              <div>
-                <Transition
-                  className={`${
-                    isOpen &&
-                    'fixed right-0 bottom-0 z-40 h-full w-[300px] bg-gray-400 drop-shadow-xl '
-                  }`}
-                  show={isOpen}
-                  enter="transition-opacity duration-500 "
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="transition-opacity"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+          <div className={`${flexBetween} gap-8  hidden md:inline-flex `}>
+            <p>sign in</p>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Become a member
+            </ActionButton>
+          </div>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className=" rounded-full bg-yellow-500 p-2 md:hidden"
+          >
+            <HiBars3 className="h-6 w-6 text-white" />
+          </button>
+          <div>
+            <Transition
+              className={`${
+                isOpen &&
+                'fixed right-0 bottom-0 z-40 h-full w-[300px] bg-gray-400 drop-shadow-xl '
+              }`}
+              show={isOpen}
+              enter="transition-opacity duration-500 "
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed top-0 left-0 w-full h-full ">
+                <Transition.Child
+                  enter="transition ease-in-out duration-500 transform"
+                  enterFrom="-translate-y-full"
+                  enterTo="translate-y-0"
+                  leave="hidden"
+                  leaveFrom="translate-y-0"
+                  leaveTo="-translate-y-full"
                 >
-                  <div className="fixed top-0 left-0 w-full h-full ">
-                    <Transition.Child
-                      enter="transition ease-in-out duration-500 transform"
-                      enterFrom="-translate-y-full"
-                      enterTo="translate-y-0"
-                      leave="hidden"
-                      leaveFrom="translate-y-0"
-                      leaveTo="-translate-y-full"
-                    >
-                      <div className="w-full  h-screen rounded-lg p-8">
-                        <div className="flex justify-end">
-                          <button onClick={() => setIsOpen(!isOpen)}>
-                            <HiXMark className="h-8 w-8 text-yellow-500" />
-                          </button>
-                        </div>
-                        <div className="flex flex-col items-center mt-10 space-y-5">
-                          <Link
-                            page="Home"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                          />
+                  <div className="w-full  h-screen rounded-lg p-8">
+                    <div className="flex justify-end">
+                      <button onClick={() => setIsOpen(!isOpen)}>
+                        <HiXMark className="h-8 w-8 text-yellow-500" />
+                      </button>
+                    </div>
+                    <div className="flex flex-col items-center mt-10 space-y-5">
+                      <Link
+                        page="Home"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                      />
 
-                          <Link
-                            page="Benefits"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                          />
+                      <Link
+                        page="Benefits"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                      />
 
-                          <Link
-                            page="Our Classes"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                          />
+                      <Link
+                        page="Our Classes"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                      />
 
-                          <Link
-                            page="Contact Us"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                          />
-                        </div>
-                      </div>
-                    </Transition.Child>
+                      <Link
+                        page="Contact Us"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                      />
+                    </div>
                   </div>
-                </Transition>
+                </Transition.Child>
               </div>
-            </div>
+            </Transition>
           </div>
         </div>
       </div>
